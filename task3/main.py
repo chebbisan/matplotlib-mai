969484f71800d4b9e781b02fd0b592dd2294677e
cheb <fngsupersteal@gmail.com>
2024-03-04 20:44:43 +0300

    style fixes

--- task3/main.py
+++ task3/main.py
@@ -6,7 +6,6 @@ D = 2
 def ackley_function(x1, x2):
     term1 = -0.02 * np.sqrt((x1**2 + x2**2) / D)
     term2 = (np.cos(2*np.pi*x1) + np.cos(2*np.pi*x2)) / D
-
     return -20 * np.exp(term1) - np.exp(term2)
 
 def main():
@@ -23,7 +22,6 @@ def main():
     ax1.set_ylabel('x2')
     ax1.set_zlabel('y=f(x1, x2)')
 
-
     ax2 = fig.add_subplot(222, projection='3d')
     ax2.plot_surface(x, y, z, cmap='Spectral')
     ax2.view_init(elev=90, azim=0)
@@ -32,8 +30,6 @@ def main():
     ax2.set_zlabel('y=f(x1, x2)')
     ax2.axes.zaxis.set_ticklabels([])
     
-
-
     ax3 = fig.add_subplot(223)
     x = np.linspace(-5, 5, 100)
     y = ackley_function(x, 0)
@@ -41,8 +37,6 @@ def main():
     ax3.set_xlabel('x1')
     ax3.set_ylabel('y=f(x1, 0)')
     
-
-
     ax4 = fig.add_subplot(224)
     x = np.linspace(-5, 5, 100)
     y = ackley_function(0, x)
@@ -52,7 +46,6 @@ def main():
     ackley_0_0 = ackley_function(0, 0)
     plt.text(-6, -19, f'Координаты тестовой точки: (0; 0)\nf(0, 0) = {ackley_0_0}')
     
-
     plt.show()
 
 main()
